d578f64894ff51b2bf4078c100ec730ffae1a734
tejaswinipxworkz <89890958+tejaswinipxworkz@users.noreply.github.com>
2021-09-21 14:37:32 +0530

    Add files via upload

---
+++ Pen.java
@@ -0,0 +1,5 @@
+package com.xworkz.oops;
+
+public class Pen {
+
+}
---
+++ Student.java
@@ -0,0 +1,20 @@
+package com.xworkz.oops;
+
+public class Student {
+	String name;
+	int rollNo;
+	int id;
+	int age;
+
+	public void speech() {
+		System.out.println("Giving speech");
+	}
+
+	public void playGame() {
+		System.out.println("Playing table tennis");
+	}
+
+	public void scienceExhibition() {
+		System.out.println("Giving exhibition of the model");
+	}
+}
